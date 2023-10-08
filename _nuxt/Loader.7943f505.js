5e573e68d7b9c80ced760b6597494be028445ac0
DanilVystupov <davaimne58@gmail.com>
2023-10-08 22:43:32 +0000

    Deploying to gh-pages from @ DanilVystupov/rick_and_morty@80713d6d81688bdc0211cb914b22307cd9bfb68d 🚀

---
+++ _nuxt/Loader.7943f505.js
@@ -0,0 +1,1 @@
+import{_ as c,o,c as s,p as _,e as a,a as t}from"./entry.f94fe60e.js";const d={},r=e=>(_("data-v-b01e9f3c"),e=e(),a(),e),n={class:"loader"},p=r(()=>t("div",{class:"loader-circle"},null,-1)),l=[p];function i(e,f){return o(),s("div",n,l)}const h=c(d,[["render",i],["__scopeId","data-v-b01e9f3c"]]);export{h as default};
